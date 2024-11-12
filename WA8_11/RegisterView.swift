695e40df1b3011f845e5bb0da6f2de7233b1c1fd
Kidus <yohannes.k@northeastern.edu>
2024-11-12 18:21:25 -0500

    Updated Mainscreenview and enabled Authentication

---
+++ WA8_11/RegisterView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterView.swift
+//  WA8_11
+//
+//  Created by Kidus Yohannes on 11/12/24.
+//
+
+import UIKit
+
+class RegisterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ WA8_11/WA8_11/Register Screen/RegisterFirebaseManager.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterFirebaseManager.swift
+//  WA8_11
+//
+//  Created by Kidus Yohannes on 11/12/24.
+//
+
+import Foundation
